8d750c06cb5cdd311535995a7a44f96f4bbd48db
PeterKellett <peterwkellett@gmail.com>
2021-11-03 12:28:12 +0000

    Added form

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     console.log("Document ready!")
     // Variable declarations
     const wcLength = 430;
@@ -10,48 +10,73 @@ $(document).ready(function(){
 
     // VW Transporter
     const transporterWidth = 1692;
-    const transporterHeight = 530;   
-    for (let i=Math.round(transporterWidth / 10) * 10; i>=0; i-=20){
-        tetaRad = Math.asin(transporterHeight/i);
-        tetaDeg = tetaRad/(Math.PI/180);
+    const transporterHeight = 530;
+    for (let i = Math.round(transporterWidth / 10) * 10; i >= 0; i -= 20) {
+        tetaRad = Math.asin(transporterHeight / i);
+        tetaDeg = tetaRad / (Math.PI / 180);
         grad = Math.tan(tetaRad);
-        casterHeight = wcLength*Math.sin(tetaRad);
+        casterHeight = wcLength * Math.sin(tetaRad);
         mechanicalSpace = transporterWidth - i;
-        $("#transporter-data").append("<tr><td>" + i + " mm <strong>/</strong> " + mechanicalSpace + " mm" + "</td><td>" + tetaDeg.toFixed(2) + " deg" + "</td><td>"+ (grad*100).toFixed(2) + " %" + "</td><td>" + casterHeight.toFixed(2) + " mm" + "</td></tr>");
+        $("#transporter-data").append("<tr><td>" + i + " mm <strong>/</strong> " + mechanicalSpace + " mm" + "</td><td>" + tetaDeg.toFixed(2) + " deg" + "</td><td>" + (grad * 100).toFixed(2) + " %" + "</td><td>" + casterHeight.toFixed(2) + " mm" + "</td></tr>");
         if (Number.isNaN(tetaDeg)) {
             break;
         }
     }
-   
+
     // VW Crafter
-    const crafterWidth = 1780;   
+    const crafterWidth = 1780;
     const crafterHeight = 670;
-    for (let i=Math.round(crafterWidth / 10) * 10; i>=0; i-=20) {
-        tetaRad = Math.asin(crafterHeight/i);
-        tetaDeg = tetaRad/(Math.PI/180);
+    for (let i = Math.round(crafterWidth / 10) * 10; i >= 0; i -= 20) {
+        tetaRad = Math.asin(crafterHeight / i);
+        tetaDeg = tetaRad / (Math.PI / 180);
         grad = Math.tan(tetaRad);
-        casterHeight = wcLength*Math.sin(tetaRad);
+        casterHeight = wcLength * Math.sin(tetaRad);
         mechanicalSpace = crafterWidth - i;
-        $("#crafter-data").append("<tr><td>" + i + " mm <strong>/</strong> " + mechanicalSpace + " mm" + "</td><td>" + tetaDeg.toFixed(2) + " deg" + "</td><td>"+ (grad*100).toFixed(2) + " %" + "</td><td>" + casterHeight.toFixed(2) + " mm" + "</td></tr>");
+        $("#crafter-data").append("<tr><td>" + i + " mm <strong>/</strong> " + mechanicalSpace + " mm" + "</td><td>" + tetaDeg.toFixed(2) + " deg" + "</td><td>" + (grad * 100).toFixed(2) + " %" + "</td><td>" + casterHeight.toFixed(2) + " mm" + "</td></tr>");
         if (Number.isNaN(tetaDeg)) {
             break;
         }
     }
 
     // Mercedes Sprinter
-    const sprinterWidth = 1738;   
+    const sprinterWidth = 1738;
     const sprinterHeight = 576;
-    for (let i=Math.round(sprinterWidth / 10) * 10; i>=0; i-=20){
-        tetaRad = Math.asin(sprinterHeight/i);
-        tetaDeg = tetaRad/(Math.PI/180);
+    for (let i = Math.round(sprinterWidth / 10) * 10; i >= 0; i -= 20) {
+        tetaRad = Math.asin(sprinterHeight / i);
+        tetaDeg = tetaRad / (Math.PI / 180);
         grad = Math.tan(tetaRad);
-        casterHeight = wcLength*Math.sin(tetaRad);
+        casterHeight = wcLength * Math.sin(tetaRad);
         mechanicalSpace = sprinterWidth - i;
-        $("#sprinter-data").append("<tr><td>" + i + " mm <strong>/</strong> " + mechanicalSpace + " mm" + "</td><td>" + tetaDeg.toFixed(2) + " deg" + "</td><td>"+ (grad*100).toFixed(2) + " %" + "</td><td>" + casterHeight.toFixed(2) + " mm" + "</td></tr>");
+        $("#sprinter-data").append("<tr><td>" + i + " mm <strong>/</strong> " + mechanicalSpace + " mm" + "</td><td>" + tetaDeg.toFixed(2) + " deg" + "</td><td>" + (grad * 100).toFixed(2) + " %" + "</td><td>" + casterHeight.toFixed(2) + " mm" + "</td></tr>");
         if (Number.isNaN(tetaDeg)) {
             break;
         }
     }
-    
 
-  });
+    $("#submit1").click(function () {
+        console.log("submit")
+        var vanWidth = $("#van-width").val();
+        var vanHeight = $("#van-height").val();
+        console.log(vanWidth)
+        console.log(vanHeight)
+        for (let i = Math.round(vanWidth / 10) * 10; i >= 0; i -= 20) {
+            tetaRad = Math.asin(vanHeight / i);
+            tetaDeg = tetaRad / (Math.PI / 180);
+            grad = Math.tan(tetaRad);
+            casterHeight = wcLength * Math.sin(tetaRad);
+            mechanicalSpace = vanWidth - i;
+            $("#ramp-data").append("<tr><td>" + i + " mm <strong>/</strong> " + mechanicalSpace + " mm" + "</td><td>" + tetaDeg.toFixed(2) + " deg" + "</td><td>" + (grad * 100).toFixed(2) + " %" + "</td><td>" + casterHeight.toFixed(2) + " mm" + "</td></tr>");
+            if (Number.isNaN(tetaDeg)) {
+                break;
+            }
+        }
+        calculate()
+    });
+
+    function calculate() {
+        console.log("calculate")
+
+    }
+
+
+});
